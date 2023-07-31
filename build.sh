515491815c39c29f30d95f0fe0b2d7d603a27ded
AnsibleGuy <guy@ansibleguy.net>
2023-07-31 19:12:09 +0200

    added information prompts

--- build.sh
+++ build.sh
@@ -5,16 +5,25 @@ set -euo pipefail
 VERSION="$1"
 FLAGS="$2"
 
+echo '### CLEANUP ###'
 rm -rf /tmp/openvpn-*
 cd /tmp
 
+echo '### DOWNLOADING SOURCES ###'
 wget "https://swupdate.openvpn.org/community/releases/openvpn-${VERSION}.tar.gz"
 tar xf "openvpn-${VERSION}.tar.gz"
 cd "/tmp/openvpn-${VERSION}"
 
+echo '### CONFIGURE ###'
 # shellcheck disable=SC2086
 ./configure $FLAGS
+echo '### MAKE ###'
 make
+echo '### MAKE INSTALL ###'
 sudo make install
+
+echo '### COPY ###'
 cp /usr/local/sbin/openvpn "/tmp/openvpn_${VERSION}"
+
+echo '### MAKE UNINSTALL ###'
 sudo make uninstall
